58eba1b6fd2a77dfa99ec11d8f9d745a6560984a
joinjoohny <joinjoohny@gmail.com>
2025-09-23 13:01:37 +0200

    Portfolio carousel

---
+++ src/data/slides.ts
@@ -0,0 +1,15 @@
+import p001 from "../assets/portfolio/p001.png";
+import p002 from "../assets/portfolio/p002.png";
+import p003 from "../assets/portfolio/p003.png";
+import p004 from "../assets/portfolio/p004.png";
+
+export const slides = [
+  { src: p001, alt: "Black & white portrait", code: "001" },
+  { src: p002, alt: "Studio portrait", code: "002" },
+  { src: p003, alt: "Outdoor fashion", code: "003" },
+  { src: p004, alt: "Editorial look", code: "004" },
+  { src: p001, alt: "Black & white portrait", code: "001" },
+  { src: p002, alt: "Studio portrait", code: "002" },
+  { src: p003, alt: "Outdoor fashion", code: "003" },
+  { src: p004, alt: "Editorial look", code: "004" },
+];
