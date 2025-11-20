d47c5c87c8c06767b0b6f243681bec151de154a1
joinjoohny <joinjoohny@gmail.com>
2025-11-20 15:50:26 +0100

    Upd photos

--- src/data/slides.ts
+++ src/data/slides.ts
@@ -13,6 +13,21 @@ import p010 from "../assets/portfolio/p010.jpg";
 import p011 from "../assets/portfolio/p011.jpg";
 import p012 from "../assets/portfolio/p012.jpg";
 
+import p013 from "../assets/portfolio/p013.jpg";
+import p014 from "../assets/portfolio/p014.jpg";
+import p015 from "../assets/portfolio/p015.jpg";
+import p016 from "../assets/portfolio/p016.jpg";
+
+import p017 from "../assets/portfolio/p017.jpg";
+import p018 from "../assets/portfolio/p018.jpg";
+import p019 from "../assets/portfolio/p019.jpg";
+import p020 from "../assets/portfolio/p020.jpg";
+
+import p021 from "../assets/portfolio/p021.jpg";
+import p022 from "../assets/portfolio/p022.jpg";
+import p023 from "../assets/portfolio/p023.jpg";
+import p024 from "../assets/portfolio/p024.jpg";
+
 export const slides = [
   { src: p001, alt: "Black & white portrait", code: "001" },
   { src: p002, alt: "Studio portrait", code: "002" },
@@ -26,4 +41,16 @@ export const slides = [
   { src: p010, alt: "Monochrome style", code: "010" },
   { src: p011, alt: "High fashion", code: "011" },
   { src: p012, alt: "Vintage look", code: "012" },
+  { src: p013, alt: "Studio portrait", code: "013" },
+  { src: p014, alt: "Studio portrait", code: "014" },
+  { src: p015, alt: "Studio portrait", code: "015" },
+  { src: p016, alt: "Vintage look", code: "016" },
+  { src: p017, alt: "Vintage look", code: "017" },
+  { src: p018, alt: "Vintage look", code: "018" },
+  { src: p019, alt: "Vintage look", code: "019" },
+  { src: p020, alt: "Vintage look", code: "020" },
+  { src: p021, alt: "Vintage look", code: "021" },
+  { src: p022, alt: "Vintage look", code: "022" },
+  { src: p023, alt: "Vintage look", code: "023" },
+  { src: p024, alt: "Vintage look", code: "024" },
 ];
